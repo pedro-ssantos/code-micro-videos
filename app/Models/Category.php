da07c6830515e93befced942c0479ceadd7eb838
Pedro Santos <peusantos@gmail.com>
2021-09-10 16:06:11 -0300

    Desafio 01.

---
+++ app/Models/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Traits\Uuid;
+
+class Category extends Model
+{
+    use SoftDeletes, Uuid;
+    
+    protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
+}
---
+++ app/Models/Traits/Uuid.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models\Traits;
+
+use Ramsey\Uuid\Uuid as RamsayUuid;
+
+trait Uuid
+{
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($obj) {
+            $obj->id = RamsayUuid::uuid4()->toString();
+        });
+    }
+}
